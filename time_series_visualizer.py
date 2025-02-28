2f0841e5f6a97a36fdeda63ca747d3984a8fcb5b
Lens Kamdem <kamdem.lens@gmail.com>
2025-02-28 21:54:28 +0000

    Added Page View Time Series Visualizer function logic

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,18 +5,30 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
-df = None
+# Calculate the 2.5th and 97.5th percentiles
+lower_bound = df['value'].quantile(0.025)
+upper_bound = df['value'].quantile(0.975)
+
+# Filter the data
+df_cleaned = df[(df['value'] >= lower_bound) & (df['value'] <= upper_bound)]
 
 
 def draw_line_plot():
     # Draw line plot
+    # Create a copy of the cleaned data
+    df_line = df_cleaned.copy()
 
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(14, 6))
+    ax.plot(df_line.index, df_line['value'], color='r', linewidth=1)
 
-
-
+    # Set titles and labels
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,12 +36,33 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df_cleaned.copy()
 
     # Draw bar plot
+    # Extract year and month from the index
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month_name()
 
+    # Group by year and month, then calculate the mean
+    df_bar = df_bar.groupby(['year', 'month'], as_index=False).mean()
 
+    # Pivot the data for easier plotting
+    df_bar = df_bar.pivot(index='year', columns='month', values='value')
 
+    # Reorder the months
+    months_order = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ]
+    df_bar = df_bar[months_order]
+
+    # Plot the data
+    fig = df_bar.plot(kind='bar', figsize=(10, 8)).get_figure()
+
+    # Set titles and labels
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(title='Months')
 
 
     # Save image and return fig (don't change this part)
@@ -44,10 +77,21 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    # Set up the figure
+    fig, axes = plt.subplots(1, 2, figsize=(18, 6))
 
+    # Year-wise box plot
+    sns.boxplot(x='year', y='value', data=df_box, ax=axes[0])
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views')
 
-
-
+    # Month-wise box plot
+    month_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    sns.boxplot(x='month', y='value', data=df_box, order=month_order, ax=axes[1])
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
